6b73829f1691c0ec0a55a9b2f0d6e057f62344cd
Rohan Yadav <rohanmaax@gmail.com>
2025-07-17 14:49:39 +0530

    Delete ZERODHA PROJECT directory

--- ZERODHA PROJECT/DASHBOARD/src/Components/Apps.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import '../App.css'
-
-const Apps = () => {
-  return <h1>Apps</h1>;
-};
-
-export default Apps;
--- ZERODHA PROJECT/DASHBOARD/src/Components/DoughnutChart.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
-import '../App.css'
-ChartJS.register(ArcElement, Tooltip, Legend);
-
-export function DoughnutChart({ data }) {
-  return <Doughnut data={data} />;
-}
